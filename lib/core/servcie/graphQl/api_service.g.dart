3152f782a67b678371c9b231488fe13ce5633932
Hussam Elmaghraby <hussamelmaghraby@gmail.com>
2024-12-22 14:07:08 +0200

    login models.

--- lib/core/servcie/graphQl/api_service.g.dart
+++ lib/core/servcie/graphQl/api_service.g.dart
@@ -6,12 +6,13 @@ part of 'api_service.dart';
 // RetrofitGenerator
 // **************************************************************************
 
-// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element,unnecessary_string_interpolations
 
 class _ApiService implements ApiService {
   _ApiService(
     this._dio, {
     this.baseUrl,
+    this.errorLogger,
   }) {
     baseUrl ??= 'https://api.escuelajs.co';
   }
@@ -19,6 +20,8 @@ class _ApiService implements ApiService {
   final Dio _dio;
 
   String? baseUrl;
+
+  final ParseErrorLogger? errorLogger;
 
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
---
+++ lib/features/auth/data/models/user_role_response/user_role_response.dart
@@ -0,0 +1,14 @@
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'user_role_response.g.dart';
+@JsonSerializable()
+class UserRoleResponse {
+  UserRoleResponse(this.userRole);
+
+  factory UserRoleResponse.fromJson(Map<String, dynamic> json) => _$UserRoleResponseFromJson(json);
+
+  @JsonKey(name: 'user')
+  final String? userRole;
+
+
+}
